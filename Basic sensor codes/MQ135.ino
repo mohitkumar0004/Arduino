913a9e5e68c921d62557e09f828e17c634a675de
Mohit kumar <50259121+mohitkumar0004@users.noreply.github.com>
2026-07-09 20:05:26 +0530

    Create MQ135.ino

---
+++ Basic sensor codes/MQ135.ino
@@ -0,0 +1,20 @@
+#define MQ135 A0 // defining pin for sensor 
+int sensorval = 0;// initializing the value to 0 to get accurate reading 
+
+void setup() {
+  pinMode(MQ135,INPUT); //LM 35 is a analog sensor from which we expect a value so we set pinmode to  INPUT so arduino can easily take input from sensor
+  Serial.begin(9600); // setting baudh rate to 9600
+ 
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  sensorval=analogRead(MQ135); //reading the input sent by sensor to microcontroller
+  Serial.println(sensorval); //printing the value on serial monitor
+  delay(1000);//delay between each print (1 second)
+  
+
+  // put your main code here, to run repeatedly:
+
+}
